58139575b51b90f0e259eb532f3d7069d07aa7bf
Gerald Devreux <gerald.devreux.pro@gmail.com>
2017-06-08 15:44:58 +0200

    remise à niveau

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@ let game = new Phaser.Game(1580, 780, Phaser.AUTO, 'game',
 var floorGroup;
 var obstacleGroup;
 var player;
-var heightMap = 2000;
+var heightMap = 3078;
 var widthMap = 4000;
+let walkLeft = true;
 
 function preload() 
 {
 	game.load.image('sand', 'assets/graphics/sand.png');
     game.load.image('ground', 'assets/graphics/ground_tile.png');
+
+    game.load.image('grass1', 'assets/graphics/ground_tile_grass1.png');
+    game.load.image('grass2', 'assets/graphics/ground_tile_grass2.png');
+    game.load.image('grass3', 'assets/graphics/ground_tile_grass3.png');
+
     game.load.spritesheet('characterAnim', 'assets/graphics/characterAnim.png', 70, 74);
     game.load.spritesheet('skeleton', 'assets/graphics/skeleton/SpriteSheets/skeleton_walk.png', 22, 33);
 
@@ -37,18 +43,20 @@ function create()
 	
 	// Init des groupes de calques
 	floorGroup = game.add.group();
+	grassGroup = game.add.group();
 	obstacleGroup = game.add.group();
 
 	floorTile = game.add.isoSprite(xt, yt, 0, 'ground', 0, floorGroup);
    	floorTile.anchor.set(0.5);
-   	player = game.add.isoSprite(350, 280, 0, 'characterAnim', 0, obstacleGroup);
    	skeleton = game.add.isoSprite(400, 300, 0, 'skeleton', 0, obstacleGroup);
    	skeleton.scale.setTo(1.5, 1.5);
+   	player = game.add.isoSprite(350, 280, 0, 'characterAnim', 0, obstacleGroup);
+   	player.scale.setTo(0.5, 0.5);
    	//skeleton = game.add.sprite(20, 20, 'skeleton');
 
 
    	skeleton.animations.add('walkRight', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 10, true);
-   	skeleton.animations.add('walkLeft', [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], 10, true);
+   	skeleton.animations.add('walkLeft', [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13], 10, true);
 
    	// Init physic
    	game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -68,23 +76,58 @@ function create()
 
     // enable physics on the player
     game.physics.isoArcade.enable(player);
-        // Permet au sprite de ne pas tomber dans le vide
+    // Permet au sprite de ne pas tomber dans le vide
     player.body.collideWorldBounds = true;
 
     game.physics.isoArcade.enable(skeleton);
     // Permet au sprite de ne pas tomber dans le vide
     skeleton.body.collideWorldBounds = true;
+    skeleton.body.velocity.x = 0;
+    skeleton.body.velocity.y = 0;
  
+ 	// generate random number
+	function rndNum(num) {
+		return Math.round(Math.random() * num);
+		
+	}
 
-    // Création du sol
-    for (var xt = 2048; xt > 0; xt -= 32) 
+    // ---------- CREATION DE LA MAP ---------- \\
+    for (var xt = 2500; xt > 0; xt -= 32) 
     {
-        for (var yt = 2048; yt > 0; yt -= 32) 
+        for (var yt = 2500; yt > 0; yt -= 32) 
         {
         	floorTile = game.add.isoSprite(xt, yt, 0, 'ground', 0, floorGroup);
         	floorTile.anchor.set(0.5);
         }
     }
+
+    	        // create the grass tiles randomly
+    var grassTile;
+    for (var xt = 2048; xt > 0; xt -= 32) {
+        for (var yt = 2048; yt > 0; yt -= 32) {
+        	
+        	var rnd = rndNum(20);
+        	
+        	if (rnd == 0) {
+        		grassTile = game.add.isoSprite(xt, yt, 0, 'grass1', 0, grassGroup);
+        		grassTile.anchor.set(0.5);
+        	}
+        	else if (rnd == 1)
+        	{
+        		grassTile = game.add.isoSprite(xt, yt, 0, 'grass2', 0, grassGroup);
+        		grassTile.anchor.set(0.5);
+        	}
+        	else if (rnd == 2)
+        	{
+        		grassTile = game.add.isoSprite(xt, yt, 0, 'grass3', 0, grassGroup);
+        		grassTile.anchor.set(0.5);
+        	}
+        	
+        	
+
+        }
+    }
+
 
     game.camera.follow(player);
 
@@ -101,13 +144,15 @@ function create()
 };
 function update() 
 {
-	//
 	player.body.velocity.x = 0;
 	player.body.velocity.y = 0;
 
-	skeleton.body.velocity.y = 0;
-	skeleton.animations.play('walkRight');
-	skeleton.body.velocity.x = 50;
+	
+	ia();
+
+	console.log(skeleton.body.x);
+
+	//console.log(skeleton.body.x);
 
 	// if(game.physics.arcade.checkCollision.right)
 	// {
